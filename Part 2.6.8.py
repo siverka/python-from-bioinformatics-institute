e54c9fc366259f216c66973cc4a40c35e0d72831
Maria Korobeynikova <siverka@gmail.com>
2017-02-01 00:29:06 +0300

    first attemp

---
+++ Part 2.6.8.py
@@ -0,0 +1,14 @@
+n = 47
+#n = int(input())
+
+res = []
+i = 1
+'''
+while len(res) < n:
+    res += str(i) * min(i, n - len(res))
+    i += 1
+print(res)
+'''
+
+#for ch in res:
+#    print(ch, end=' ')
