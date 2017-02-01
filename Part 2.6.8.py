0f736bf0db593d928ad65fea2177c159ceaab3ad
Maria Korobeynikova <siverka@gmail.com>
2017-02-01 22:20:14 +0300

    resolved task 2.6.8

--- Part 2.6.8.py
+++ Part 2.6.8.py
@@ -1,14 +1,11 @@
-n = 47
-#n = int(input())
+#n = 47
+n = int(input())
 
 res = []
 i = 1
-'''
 while len(res) < n:
-    res += str(i) * min(i, n - len(res))
+    res += [str(i)] * min(i, n - len(res))
     i += 1
-print(res)
-'''
 
-#for ch in res:
-#    print(ch, end=' ')
+for ch in res:
+    print(ch, end=' ')
