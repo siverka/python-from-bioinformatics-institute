738f7544d215b3686de8fe7cbecbe438f0da3fc8
Maria Korobeynikova <siverka@gmail.com>
2017-02-05 18:59:34 +0300

    continue works on task

--- Part 3.4.2.py
+++ Part 3.4.2.py
@@ -1,6 +1,7 @@
 with open('input_3.4.2.txt') as file:
-    line = file.readline().strip()
-    print(line)
+    for line in file:
+        line = line.strip()
+        print(line)
 
 
 inline = 'a3b4c2e10b1'
