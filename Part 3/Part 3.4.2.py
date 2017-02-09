49a879ca8833eaa6a392ed0c313ce2b8d4c47e8a
Maria Korobeynikova <siverka@gmail.com>
2017-02-10 00:41:09 +0300

    resolved task 3.4.2

--- Part 3/Part 3.4.2.py
+++ Part 3/Part 3.4.2.py
@@ -1,7 +1,20 @@
+line = ''
 with open('input_3.4.2.txt') as file:
-    for line in file:
-        line = line.strip()
-        print(line)
+    line = file.readline().strip()
 
+res = ''
+i = 0
+while i < len(line):
+    char = line[i]
+    j = 1
+    while i + j < len(line):
+        if line[i+j].isdigit():
+            j += 1
+        else:
+            break
+    num = int(''.join(line[i+1:i+j]))
+    i += j
+    res += char * num
 
-inline = 'a3b4c2e10b1'
+with open('output_3.4.2.txt', 'w') as file:
+    file.writelines(res)
