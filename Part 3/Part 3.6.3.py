172ccfe349bbfdf41a14711ceb57ad43a25a6be5
Maria Korobeynikova <siverka@gmail.com>
2017-02-10 21:15:25 +0300

    resolved task 3.6.3

--- Part 3/Part 3.6.3.py
+++ Part 3/Part 3.6.3.py
@@ -1,7 +1,12 @@
 import requests
-import os
 
+name = '699991.txt'
 address = 'https://stepic.org/media/attachments/course67/3.6.3/'
-os.path.join()
 
-response = requests.get(address)
+name = requests.get(url=(address+name)).text
+while name[:2] != 'We':
+    name = requests.get(url=(address+name)).text
+    print(name)
+
+with open("output_3.6.3.txt", "w") as file:
+    file.writelines(name)
